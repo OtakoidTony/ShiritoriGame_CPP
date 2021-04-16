cce225a399d257664e3f7b028f901873d6843c5e
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2021-04-16 18:04:02 +0900

    update

--- shiritori.h
+++ shiritori.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-namespace Shiritori{
+namespace Shiritori {
     enum CheckResult {
         SAFE_WORD,
         FIRST_WORD,
@@ -16,14 +16,19 @@ namespace Shiritori{
         UNAVAILABLE_WORD
     };
 
-    class Player{
+    class Player {
         string name;
     public:
         Player() = default;
+
         explicit Player(string name);
+
         void setName(string name);
+
         string getName();
+
         friend bool operator==(const Player &p1, const Player &p2);
+
         friend bool operator!=(const Player &p1, const Player &p2);
     };
 
@@ -36,13 +41,21 @@ namespace Shiritori{
         list<string> wordlist;
     public:
         explicit Game(int headcount);
+
         int getHeadcount() const;
+
         void setPlayer(int index, Player player);
+
         Player getPlayer(int index) const;
+
         void go(Player &player, const string &word);
+
         int check(const Player &player, string word);
+
     private:
         bool isAvailableWord(const string &word);
+
         bool isUsed(const string &word);
+    };
 }
 #endif //SHIRITORIGAME_SHIRITORI_H
